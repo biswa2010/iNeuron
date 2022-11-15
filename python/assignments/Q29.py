0f50e4aca359f37caa501920066907d97b4bc908
Biswaranjan Adhikary <biswaranjanadhikary10@gmail.com>
2022-11-15 08:54:24 +0530

    slice usage
    Write a code to get the desired output of the following

---
+++ python/assignments/Q29.py
@@ -0,0 +1,11 @@
+
+#Q29. Write a code to get the desired output of the following
+'''
+string = "Big Data iNeuron"
+desired_output = "norueNi"
+'''
+
+string = "Big Data iNeuron"
+desired_output = string[9:]
+
+print(desired_output)
