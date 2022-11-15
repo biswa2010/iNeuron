c1ae6687a0ce5da9de58d86692900c107d89e964
Biswaranjan Adhikary <biswaranjanadhikary10@gmail.com>
2022-11-15 08:09:41 +0530

    Why is Python called a dynamically typed language

---
+++ python/assignments/Q2.py
@@ -0,0 +1,5 @@
+#Q2. Why is Python called a dynamically typed language?
+
+""" 
+Python is known as dynamically typed language because we don't mention any variable type and the type is determined during runtime.
+"""
