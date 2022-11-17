5a7b4f6058e548eaf952dbf9e75c45205ea32f54
Biswaranjan Adhikary <biswaranjanadhikary10@gmail.com>
2022-11-17 19:43:09 +0530

    Q30

--- python/assignments/assignment2_Q26-Q100.py
+++ python/assignments/assignment2_Q26-Q100.py
@@ -20,4 +20,13 @@ desired_output = string[-1:-8:-1]
 
 print(desired_output)
 '''
+#Q30. Resverse the string given in the above question.
+
+'''
+string = "Big Data iNeuron"
+'''
+string = "Big Data iNeuron"
+desired_output = string[-1::-1]
+
+print(desired_output)
 
