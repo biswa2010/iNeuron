f27a4b909ba412c2e5c6c2ca7713ae9d67f88364
Biswaranjan Adhikary <biswaranjanadhikary10@gmail.com>
2022-11-17 19:40:03 +0530

    Q29

--- python/assignments/assignment2_Q26-Q100.py
+++ python/assignments/assignment2_Q26-Q100.py
@@ -8,3 +8,16 @@ string = "Big Data iNeuron"
 desired_output = string[9:]
 
 print(desired_output)
+
+#Q29. Write a code to get the desired output of the following
+'''
+string = "Big Data iNeuron"
+desired_output = "norueNi"
+'''
+'''
+string = "Big Data iNeuron"
+desired_output = string[-1:-8:-1]
+
+print(desired_output)
+'''
+
