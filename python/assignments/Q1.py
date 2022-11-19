4954b38938a56b994b2396b6ed39cfc15eb86b40
Biswaranjan Adhikary <biswaranjanadhikary10@gmail.com>
2022-11-19 09:52:09 +0530

    delete

--- python/assignments/Q1.py
+++
@@ -1,9 +0,0 @@
-#Q1. Why do we call Python as a general purpose and high-level programming language?
-
-"""
-
-Python doesn't interact with the machine directly with the machine rather through the interpreter which makes it a HLL. 
-Also python is known as a general purpose programming language as it's not for any specific problems but we can use this for
-ML, AI, data engineering, software development etc.
-
-"""
